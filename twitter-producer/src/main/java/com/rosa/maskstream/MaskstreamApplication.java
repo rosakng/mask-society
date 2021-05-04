0923924ebd0bbfcc14d57a96cad27847cbbf0908
rosa <rosakang12@gmail.com>
2021-05-04 18:24:42 -0400

    Initial Spring Project commit

---
+++ twitter-producer/src/main/java/com/rosa/maskstream/MaskstreamApplication.java
@@ -0,0 +1,13 @@
+package com.rosa.maskstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaskstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaskstreamApplication.class, args);
+	}
+
+}
