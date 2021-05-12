25310a808986e76c0b817721be54c1f42b8583c1
rosa <rosakang12@gmail.com>
2021-05-11 23:38:33 -0400

    wip create rest endpoint

---
+++ twitter-events-processor/src/main/java/com/rosa/maskstream/rest/TweetSimilarity.java
@@ -0,0 +1,16 @@
+package com.rosa.maskstream.rest;
+
+import com.datastax.driver.mapping.annotations.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "")
+public class TweetSimilarity {
+    private String location_similarity;
+    private int num_occurrences;
+}
