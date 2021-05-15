0af7d544e337f9f2112807596f9c5265cd52e712
rosa <rosakang12@gmail.com>
2021-05-15 19:16:28 -0400

    add simple rest endpoints

--- locality/src/main/java/com/maskstream/locality/repository/TweetRepository.java
+++ locality/src/main/java/com/maskstream/locality/repository/TweetRepository.java
@@ -1,7 +1,8 @@
 package com.maskstream.locality.repository;
 
 import com.maskstream.locality.model.SimilarityScore;
+import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TweetRepository extends CrudRepository<SimilarityScore, String> {
+public interface TweetRepository extends CassandraRepository<SimilarityScore, String> {
 }
--- locality/src/main/java/com/maskstream/locality/rest/Controller.java
+++ locality/src/main/java/com/maskstream/locality/rest/Controller.java
@@ -3,11 +3,15 @@ package com.maskstream.locality.rest;
 import com.maskstream.locality.model.SimilarityScore;
 import com.maskstream.locality.repository.TweetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class Controller {
@@ -20,10 +24,29 @@ public class Controller {
     }
 
     @GetMapping("/locations")
-    public List<SimilarityScore> getScores() {
-        Iterable<SimilarityScore> result = tweetRepository.findAll();
-        List<SimilarityScore> employeesList = new ArrayList<>();
-        result.forEach(employeesList::add);
-        return employeesList;
+    public ResponseEntity<List<SimilarityScore>> getAllLocationsAndScores() {
+        try {
+            List<SimilarityScore> result = tweetRepository.findAll();
+            if (result.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            List<SimilarityScore> employeesList = new ArrayList<>();
+            result.forEach(employeesList::add);
+            return new ResponseEntity<>(employeesList, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/locations/{key}")
+    public ResponseEntity<SimilarityScore> getByLocationSimilarity(@PathVariable("key") String key) {
+        try {
+            Optional<SimilarityScore> similarityScoreData = tweetRepository.findById(key);
+            return similarityScoreData
+                    .map(similarityScore -> new ResponseEntity<>(similarityScore, HttpStatus.OK))
+                    .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
